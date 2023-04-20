07df615eddc0507eafd7e8acea33bde696950338
Suraphop <suraphop.bunsawat@gmail.com>
2023-04-20 20:35:14 +0700

    init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,54 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [img, setImg] = useState();
   const [count, setCount] = useState(0);
+  const MINUTE_MS = 5000;
+  const getPosts = async () => {
+    try {
+      const res = await axios.get(
+        "http://localhost:8000/cp", { responseType: 'blob' });
+        const imageObjectURL: any  = URL.createObjectURL(res.data);
+       
+        setImg(imageObjectURL)
+      // const keys = res.data.results[0].series[0].columns;
+      // const values = res.data.results[0].series[0].values[0];
+
+      // var obj = [];
+
+      // //for each loop
+      // keys.forEach((ele, i) => {
+      //   obj[ele] = values[i];
+      // });
+
+      // setDatas(obj);
+      // //print the obj
+      // console.log(obj);
+    } catch (err) {
+      console.log(`ERROR: ${err}`);
+    }
+ 
+  };
+  useEffect(() => {
+    getPosts();
+    const interval = setInterval(() => {
+      console.log("Logs every minute");
+      getPosts();
+    }, MINUTE_MS);
+
+    return () => clearInterval(interval); // This represents the unmount function, in which you need to clear your interval to prevent memory leaks.
+  }, []);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+   
+      <h1>CPK realtime </h1>
+      <img src={img} alt="" />
+  
     </div>
   );
 }
